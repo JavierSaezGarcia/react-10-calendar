10584119609a09276a5cb4eb0ae07f1a5d96652c
javiersaezgarcia <banbleac@gmail.com>
2023-06-29 12:14:31 +0200

    update c2j

--- public/assets/index-ae1ea3c2.js
+++ public/assets/index-ae1ea3c2.js
@@ -2,7 +2,7 @@ var UO=(e,t)=>()=>(t||e((t={exports:{}}).exports,t),t.exports);var KY=UO((tn,nn)
  * @license React
  * react.production.min.js
  *
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) Facebook, Inc. and its affiliates
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
